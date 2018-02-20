d607786b025208e78ba11e7d36ae2406ab41adbc
yutianhuihong <2256912385@qq.com>
2018-02-20 15:37:54 +0800

    module like
    rubbish implementation

--- js/AI.js
+++ js/AI.js
@@ -1,8 +1,13 @@
-//alert("this is ai");
-const Constants = {
+// for judgement convenience
+// let wrappedBoard = [];
+let StepGenerator = (function(){
+
+let StepGenerator = {};
+
+let Constants = {
     ChessBoard:{
         ROW_NUM:15,
-        COL_NUM:15
+        COL_NUM:15,
     },
     Chesspiece:{
         BLACK: 1,
@@ -11,23 +16,10 @@ const Constants = {
     }
 };
 
-// for judgement convenience
-// let wrappedBoard = [];
-
-function wrapBoard(board){
-    let padding = new Array(Constants.ChessBoard.COL_NUM + 4).fill(
-        Constants.Chesspiece.EMPTY);
-    for(let row of board){
-        row.unshift(Constants.Chesspiece.EMPTY, Constants.Chesspiece.EMPTY);
-        row.push(Constants.Chesspiece.EMPTY, Constants.Chesspiece.EMPTY);
-    }
-    return [padding, padding, ...board, padding, padding];
-}
-
 let UtilMethods = {
     // wrappedBoard
     isPositionEmpty(board, rowPos, colPos){
-        return (board[rowPos][colPos] === 'e');
+        return (board[rowPos][colPos] === Constants.Chesspiece.EMPTY);
     },
     // like this 'b' 'b'
     hasOneStepNeighbour(board, color, rowPos, colPos){
@@ -51,8 +43,30 @@ let UtilMethods = {
             (board[rowPos+2][colPos] == color) ||
             (board[rowPos+2][colPos+2] == color);
     }
-};
+}
 
+StepGenerator.Constants = Constants;
+StepGenerator.UtilMethods = UtilMethods;
+
+StepGenerator.copyAndWrapBoard = (function(){
+    let padding = new Array(Constants.ChessBoard.COL_NUM + 4).fill(
+        Constants.Chesspiece.EMPTY);
+
+    return function(board){
+        let res = [padding, padding];
+        for(let row of board){
+            res.push([
+                Constants.Chesspiece.EMPTY, 
+                Constants.Chesspiece.EMPTY, 
+                ...row, 
+                Constants.Chesspiece.EMPTY,
+                Constants.Chesspiece.EMPTY
+            ]);
+        }
+        res.push(padding, padding);
+        return res;
+    }
+})();
 
 // return type
 // [
@@ -60,22 +74,27 @@ let UtilMethods = {
 // ]
 
 // range: [0,15)
-function generateAllNextPossibleMove(wrappedBoard, color){
+StepGenerator.generateAllNextPossibleMove = function(wrappedBoard, color){
     let oneStepNeighbours = [],
         twoStepNeighbours = [];
     
     let rowEnd = Constants.ChessBoard.ROW_NUM + 2,
         colEnd = Constants.ChessBoard.COL_NUM + 2;
+    console.log(rowEnd, colEnd);
     for(let i = 2; i < rowEnd;i++){
         for(let j = 2;j < colEnd;j++){
             if(UtilMethods.isPositionEmpty(wrappedBoard, i, j)){
                 if(UtilMethods.hasOneStepNeighbour(wrappedBoard, color, i, j)){
-                    oneStepNeighbours.push([i-2, j-2]);
+                    oneStepNeighbours.push([i, j]);
                 }else if(UtilMethods.hasTwoStepNeighbour(wrappedBoard, color, i, j)){
-                    twoStepNeighbours.push([i-2, j-2]);
+                    twoStepNeighbours.push([i, j]);
                 }
             }
         }
     }
     return [...oneStepNeighbours, ...twoStepNeighbours];
 }
+
+return StepGenerator;
+
+})();
