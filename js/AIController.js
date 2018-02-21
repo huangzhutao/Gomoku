d5988d09aa0edaa34e956d14b0c26ec1f7aeb429
yutianhuihong <2256912385@qq.com>
2018-02-21 20:43:03 +0800

    1

--- js/AIController.js
+++ js/AIController.js
@@ -6,22 +6,29 @@ generateAllNextPossibleMove(wrapBoard(matrix), color) //落子位置函数
 
 */
 
-const MIN = -4294967295;
-const MAX = 4294967295;
-var alpha = 0,
-	beta = 0;
+const MIN = Number.NEGATIVE_INFINITY;
+const MAX = Number.POSITIVE_INFINITY;
+
+var alpha = Number.NEGATIVE_INFINITY,
+	beta = Number.POSITIVE_INFINITY;
+
+var pointCounter = 0;
 
 //board 当前棋盘, deep 思考步数
 var FunctionMaxMin = function(board, color, deep) {
 	var best = MIN;
 	//console.log(board);
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var boardTemp = StepGenerator.copyAndWrapBoard(board);
+	var points = StepGenerator.generateAllNextPossibleMove(boardTemp, color);
 	var bestPoints = [];
 	//console.log(points);
 	for(var i = 0; i < points.length; i++){
 		var now_point = points[i];
-		board[now_point[0]][now_point[1]] = color;         //下子的颜色
-		var value = min(board, Math.abs(color-1), alpha, beta, deep-1);
+		boardTemp[now_point[0]+2][now_point[1]+2] = color;         //下子的颜色
+
+		var value = min(boardTemp, Math.abs(color-1), alpha, beta, deep-1);
+
+		pointCounter++;
 
 		if(value == best){
 			bestPoints.push(now_point);
@@ -34,9 +41,9 @@ var FunctionMaxMin = function(board, color, deep) {
 		else{
 			console.log('ignore');
 		}
-		board[now_point[0]][now_point[1]] = 'e'; 
+		boardTemp[now_point[0]+2][now_point[1]+2] = 'e'; 
 	}
-	console.log(bestPoints.length);
+	console.log("搜索节点数："+pointCounter);
 	var result = bestPoints[Math.floor(Math.random() * bestPoints.length)];
 	if(result == undefined)
 		return [];
@@ -50,15 +57,22 @@ var max = function(board, color, alpha, beta, deep){
 		return v;
 
 	var best = MIN;
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var points = StepGenerator.generateAllNextPossibleMove(board, color);
 
 	for( var i = 0; i < points.length; i++){
+
+		pointCounter++;
+
 		var p = points[i];
-		board[p[0]][p[1]] = color;
-		var v = min(board, Math.abs(color-1), alpha, beta, deep-1);
-		board[p[0]][p[1]] = 'e';
+		board[p[0]+2][p[1]+2] = color;
+		var v = min(board, Math.abs(color-1), alpha, best > beta? best : beta, deep-1);
+		board[p[0]+2][p[1]+2] = 'e';
 		if(v > best)
 			best = v;
+		if(v > alpha)  
+			break;
+		//alpha：上一层（min层）的当前最小值，v：当前层（mx层）的下一层的最小值
+		/*我方在当前位置的下子，应当使对方紧接着的下子所产生的优势不超过对方上一步下子所产生的优势*/
 	}
 
 	return best;
@@ -71,15 +85,22 @@ var min = function(board, color, alpha, beta, deep){
 		return v;
 
 	var best = MAX;
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var points = StepGenerator.generateAllNextPossibleMove(board, color);
 
 	for( var i = 0; i < points.length; i++){
+
+		pointCounter++;
+
 		var p = points[i];
-		board[p[0]][p[1]] = color;
-		var v = max(board, Math.abs(color-1), alpha, beta, deep-1);
-		board[p[0]][p[1]] = 'e';
+		board[p[0]+2][p[1]+2] = color;
+		var v = max(board, Math.abs(color-1), best < alpha? best : alpha, beta, deep-1);
+		board[p[0]+2][p[1]+2] = 'e';
 		if(v < best)
 			best = v;
+		if(v < beta)
+			break;
+		//beta：上一层（max层）的当前最大值，v：当前层（min层）的下一层的最大值
+		/*对方在当前位置的下子，应当使我方紧接着的下子所产生的优势超过我方上一步下子所产生的优势*/
 	}
 
 	return best;
